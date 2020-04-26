e5b541d2afc7a524778144dbd078c1438102e54e
BanariVlad <55697871+BanariVlad@users.noreply.github.com>
2020-04-26 17:29:51 +0300

    Vue

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -2,9 +2,16 @@
   <div class="d-flex" style="height: 100vh">
     <v-container 
       class="align-self-center" 
-      style="width: 300px"
+      style="width: 350px"
     >
       <v-row>
+        <v-btn 
+          dark depressed 
+          class="cyan clear" 
+          @click="clear()"
+        >
+          Clear
+        </v-btn>
         <p class="subheading text-right">20 + 107</p>
         <p class="display-1 text-right">127</p>
       </v-row>
@@ -14,7 +21,12 @@
           :key="operation"
           class="text-center"
         >
-          <button class="button">{{operation}}</button>
+          <button 
+            class="button"
+            @click="updateCurrentNumber(operation)"
+          >
+            {{operation}}
+          </button>
         </v-col>
       </v-row>
       <v-row 
@@ -28,7 +40,12 @@
           style="height: 75px"
           class="text-center"
         >
-         <button :class='number === numbers[numbers.length - 1] ? "equal" : "button"'>{{number}}</button>
+         <button
+            :class='number === numbers[numbers.length - 1] ? "equal" : "button"'
+            @click="addDigit(number)"
+          >
+            {{number}}
+          </button>
         </v-col>
       </v-row>
     </v-container>
@@ -40,8 +57,73 @@ export default {
   data() {
     return {
       numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, '.', 0, '='],
-      operations: ['/', '*', '-', '+']
+      operations: ['/', '*', '-', '+'],
+      currentOperation: '',
+      currentNumber: '',
+      prevNumber: ''
     };
+  },
+  methods: {
+    addDigit(newNumber) {
+      let currentNumberOut = document.querySelector('p.display-1');
+      let prevNumberOut = document.querySelector('p.subheading');
+      if (newNumber !== '=' && this.currentNumber.length < 12) {
+        if (newNumber === '.' && this.currentNumber.includes('.')) {
+          return;
+        }
+        if (newNumber === '.' && this.currentNumber.length === 0) {
+          this.currentNumber = '0';
+        }
+        this.currentNumber += newNumber;
+        currentNumberOut.innerText = this.currentNumber;
+      } else if (newNumber === '=') {
+        this.updateCurrentNumber(this.currentOperation);
+        prevNumberOut.innerText = '';
+        currentNumberOut.innerText = this.prevNumber;
+      }
+    },
+
+    updateCurrentNumber(operation) {
+      let currentNumberOut = document.querySelector('p.display-1');
+      let prevNumberOut = document.querySelector('p.subheading');
+      if (!this.currentOperation) {
+        prevNumberOut.innerText = this.currentNumber;
+        currentNumberOut.innerText = '';
+        this.prevNumber = this.currentNumber;
+        this.currentNumber = '';
+        this.currentOperation = operation;
+        return;
+      }
+      switch(this.currentOperation) {
+        case '+':
+          this.prevNumber = Number(this.prevNumber) + Number(this.currentNumber);
+        break;
+        case '-':
+          this.prevNumber = Number(this.prevNumber) - Number(this.currentNumber);
+        break;
+        case '*':
+          this.prevNumber = Number(this.prevNumber) * Number(this.currentNumber);
+        break;
+        case '/':
+          if (this.currentNumber === '0') {
+            this.prevNumber = 'Error';
+          } else {
+            this.prevNumber = Number(this.prevNumber) / Number(this.currentNumber);
+          }
+        break;
+      }
+      prevNumberOut.innerText = this.prevNumber;
+      currentNumberOut.innerText = '';
+      this.currentNumber = '';
+      this.currentOperation = operation;
+    }, 
+
+    clear() {
+      document.querySelector('p.display-1').innerText = '';
+      document.querySelector('p.subheading').innerText = '';
+      this.currentNumber = '';
+      this.prevNumber = '';
+    }
   }
 };
 </script>
@@ -75,6 +157,10 @@ export default {
     padding-top: 30px;
   }
 
+  .clear {
+    top: 36px;
+    border-radius: 0;
+  }
   .equal {
     background-color: orange;
   }
